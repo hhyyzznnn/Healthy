cb5835d8a44425b94dc976e3da4ff38b47ce0b15
예준 <yejun4831@gmail.com>
2024-11-05 23:53:09 +0900

    [feat] BottomNavigationBar 구현
    TabBarView 이용해서 화면 전환

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,75 @@
-import 'package:flutter/material.dart';
-import 'constants/colors.dart';
+  import 'package:flutter/material.dart';
+  import 'constants/colors.dart';
 
-void main() async {
-  runApp(MainPage());
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _index = 0;
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 4, vsync: this);
-    _tabController.addListener(tabListener);
+  void main() async {
+    runApp(MainPage());
   }
 
-  @override
-  void dispose() {
-    _tabController.removeListener(tabListener);
+  class NavItem {
+    final IconData activeIcon;
+    final String label;
 
-    super.dispose();
+    const NavItem({
+      required this.activeIcon,
+      required this.label
+  });
   }
 
-  void tabListener() {
-    setState(() {
-      _index = _tabController.index;
-    });
+  const _navItems = [
+    NavItem(activeIcon: Icons.home, label: "홈"),
+    NavItem(activeIcon: Icons.restaurant, label: "식단"),
+    NavItem(activeIcon: Icons.fitness_center, label: "운동"),
+    NavItem(activeIcon: Icons.person, label: "프로필")
+  ];
+
+  class MainPage extends StatefulWidget {
+    const MainPage({super.key});
+
+    @override
+    State<MainPage> createState() => _MainPageState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (int index) {
-          _tabController.animateTo(index);
-        },
-        currentIndex: _index,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant),
-            label: '식단',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: '운동',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '프로필',
-          ),
-        ],
-      ),
-      child: TabBarView(
-        physics: const NeverScrollableScrollPhysics(),
-        controller: _tabController,
-        children: const [],
-      )
-    );
+  class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+    int _index = 0;
+    late TabController _tabController;
+
+    @override
+    void initState() {
+      super.initState();
+      _tabController = TabController(length: _navItems.length, vsync: this);
+      _tabController.addListener(tabListener);
+    }
+
+    @override
+    void dispose() {
+      _tabController.removeListener(tabListener);
+
+      super.dispose();
+    }
+
+    void tabListener() {
+      setState(() {
+        _index = _tabController.index;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (int index) {
+            _tabController.animateTo(index);
+          },
+          currentIndex: _index,
+          items: _navItems.map((item) {
+            return BottomNavigationBarItem(icon: Icon(item.activeIcon), label: item.label);
+          }).toList()
+        ),
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _tabController,
+          children: const [],
+        )
+      );
+    }
   }
-}
