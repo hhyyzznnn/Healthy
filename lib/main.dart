bf5601d813898f3a23ba8f0e61573b72c32d97af
예준 <yejun4831@gmail.com>
2024-11-11 00:31:44 +0900

    [Feat] 필요 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,70 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:healthy/presentation/screens/diet.dart';
 import 'package:healthy/presentation/screens/home.dart';
 import 'package:healthy/presentation/screens/profile.dart';
 import 'package:healthy/presentation/screens/work_out.dart';
-  import 'constants/colors.dart';
+import 'constants/colors.dart';
 
-  void main() async {
-    runApp(MaterialApp(home: MainPage()));
+void main() async {
+  runApp(MaterialApp(home: MainPage()));
+}
+
+class NavItem {
+  final IconData activeIcon;
+  final String label;
+
+  const NavItem({required this.activeIcon, required this.label});
+}
+
+const _navItems = [
+  NavItem(activeIcon: Icons.home, label: "홈"),
+  NavItem(activeIcon: Icons.restaurant, label: "식단"),
+  NavItem(activeIcon: Icons.fitness_center, label: "운동"),
+  NavItem(activeIcon: Icons.person, label: "프로필")
+];
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  int _index = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: _navItems.length, vsync: this);
+    _tabController.addListener(tabListener);
   }
 
-  class NavItem {
-    final IconData activeIcon;
-    final String label;
+  @override
+  void dispose() {
+    _tabController.removeListener(tabListener);
 
-    const NavItem({
-      required this.activeIcon,
-      required this.label
-  });
+    super.dispose();
   }
 
-  const _navItems = [
-    NavItem(activeIcon: Icons.home, label: "홈"),
-    NavItem(activeIcon: Icons.restaurant, label: "식단"),
-    NavItem(activeIcon: Icons.fitness_center, label: "운동"),
-    NavItem(activeIcon: Icons.person, label: "프로필")
-  ];
-
-  class MainPage extends StatefulWidget {
-    const MainPage({super.key});
-
-    @override
-    State<MainPage> createState() => _MainPageState();
+  void tabListener() {
+    setState(() {
+      _index = _tabController.index;
+    });
   }
 
-  class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
-    int _index = 0;
-    late TabController _tabController;
-
-    @override
-    void initState() {
-      super.initState();
-      _tabController = TabController(length: _navItems.length, vsync: this);
-      _tabController.addListener(tabListener);
-    }
-
-    @override
-    void dispose() {
-      _tabController.removeListener(tabListener);
-
-      super.dispose();
-    }
-
-    void tabListener() {
-      setState(() {
-        _index = _tabController.index;
-      });
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         bottomNavigationBar: BottomNavigationBar(
           onTap: (int index) {
             _tabController.animateTo(index);
           },
           currentIndex: _index,
           items: _navItems.map((item) {
-            return BottomNavigationBarItem(icon: Icon(item.activeIcon), label: item.label);
+            return BottomNavigationBarItem(
+                icon: Icon(item.activeIcon), label: item.label);
           }).toList(),
           selectedItemColor: AppColors.primaryBlue,
           unselectedItemColor: Colors.grey,
@@ -83,7 +81,6 @@ import 'package:healthy/presentation/screens/work_out.dart';
             DietScreen(),
             ProfileScreen()
           ],
-        )
-      );
-    }
+        ));
   }
+}
